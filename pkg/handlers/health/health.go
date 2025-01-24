b492cf6497b2944aaef00923455e34aeaf1c7e36
camaeel <12999736+camaeel@users.noreply.github.com>
2025-01-24 09:09:05 +0100

    debug: more debug for readyz

--- pkg/handlers/health/health.go
+++ pkg/handlers/health/health.go
@@ -3,6 +3,7 @@ package health
 import (
 	"database/sql"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func Healthz(c *gin.Context) {
@@ -14,13 +15,16 @@ func Healthz(c *gin.Context) {
 func Readyz(c *gin.Context) {
 	value, exists := c.Get("db")
 	if !exists {
+		log.Printf("Db not found in the context")
 		c.Abort()
 	}
 	db := value.(*sql.DB)
 	_, err := db.Exec("SELECT 1 FROM notes")
 	if err != nil {
+		log.Printf("ERROR: Database query failed with: %v", err)
 		c.AbortWithError(503, err)
 	} else {
+		log.Printf("Readiness probe ok")
 		c.JSON(200, gin.H{
 			"message": "ok",
 		})
